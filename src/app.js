25b2cf04c40a83a0b17379211f817744f4ab66b2
PhilippoRodrigues <philippo.rodrigues@al.infnet.edu.br>
2020-04-16 23:32:47 -0300

    first commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
+const { uuid, isUuid } = require("uuidv4");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +9,124 @@ app.use(cors());
 
 const repositories = [];
 
+//Validation Id Middleware
+
+function validateRepositoryId(request, response, next) {
+
+  const {
+    id
+  } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({
+      error: 'Invalid project ID.'
+    });
+  }
+  return next();
+}
+
+app.use('/projects/:id', validateRepositoryId);
+
+//Method GET - List respositories
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  const {title} = request.query;
+  const results = title ? repositories.filter(repo => repo.title.includes(title)) :
+    repositories;
+
+  return response.json(results);
 });
+
+//Method POST - Create repository
 
 app.post("/repositories", (request, response) => {
-  // TODO
+
+  const {
+    title,
+    url,
+    techs,
+    likes = 0
+  } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
+
+//Method PUT - edit/update repository
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+
+  const { id } = request.params;
+
+  const {
+    title,
+    url,
+    techs,
+  } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({error: "Repository not found"});
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes:repositories[repositoryIndex].likes
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
+
+//Method DELETE - delete repository
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({error: "Repository not found"});
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
+// Likes - increase number of likes + 1
+
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+
+  const {id} = request.params;
+ 
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({
+      error: "Repository not found"
+    });
+  }
+
+  const repository = repositories[repositoryIndex];
+
+  repository.likes += 1;
+
+  return response.json(repository);
 });
 
 module.exports = app;
